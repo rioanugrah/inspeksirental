dfdd4e808aaddf52c53b51240c9baaa1f266640e
Rio Anugrah Adam Saputra <rioanugrah999@gmail.com>
2024-07-06 20:05:23 +0700

    Inspeksi Rental

---
+++ resources/views/layouts/backend/master.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    @php
+        $asset = asset('backend/assets/');
+    @endphp
+    <link href="{{ $asset }}/libs/flatpickr/flatpickr.min.css" rel="stylesheet" type="text/css" />
+
+    <!-- App css -->
+    <link href="{{ $asset }}/css/bootstrap.min.css" rel="stylesheet" type="text/css" id="bs-default-stylesheet" />
+    <link href="{{ $asset }}/css/app.min.css" rel="stylesheet" type="text/css" id="app-default-stylesheet" />
+
+    <link href="{{ $asset }}/css/bootstrap-dark.min.css" rel="stylesheet" type="text/css" id="bs-dark-stylesheet"
+        disabled />
+    <link href="{{ $asset }}/css/app-dark.min.css" rel="stylesheet" type="text/css" id="app-dark-stylesheet"
+        disabled />
+
+    <!-- icons -->
+    <link href="{{ $asset }}/css/icons.min.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body class="loading"
+    data-layout='{"mode": "light", "width": "fluid", "menuPosition": "fixed", "sidebar": { "color": "light", "size": "default", "showuser": false}, "topbar": {"color": "light"}, "showRightSidebarOnPageLoad": true}'>
+    <!-- Begin page -->
+    <div id="wrapper">
+        @include('layouts.backend.topbar')
+        @include('layouts.backend.sidebar')
+        <div class="content-page">
+            <div class="content">
+                <div class="container-fluid">
+                    @yield('content')
+                </div>
+            </div>
+            @include('layouts.backend.footer')
+        </div>
+    </div>
+    <script src="{{ $asset }}/js/vendor.min.js"></script>
+
+    <script src="{{ $asset }}/libs/moment/min/moment.min.js"></script>
+    <script src="{{ $asset }}/libs/apexcharts/apexcharts.min.js"></script>
+    <script src="{{ $asset }}/libs/flatpickr/flatpickr.min.js"></script>
+
+    <script src="{{ $asset }}/js/pages/dashboard.init.js"></script>
+
+    <script src="{{ $asset }}/js/app.min.js"></script>
+</body>
+
+</html>
---
+++ resources/views/layouts/backend/sidebar.blade.php
@@ -0,0 +1,59 @@
+<div class="left-side-menu">
+
+    <div class="h-100" data-simplebar>
+
+        <!-- User box -->
+        <div class="user-box text-center">
+            <img src="assets/images/users/avatar-1.jpg" alt="user-img" title="Mat Helme"
+                class="rounded-circle avatar-md">
+            <div class="dropdown">
+                <a href="javascript: void(0);" class="text-dark dropdown-toggle h5 mt-2 mb-1 d-block"
+                    data-bs-toggle="dropdown">{{ auth()->user()->name }}</a>
+                <div class="dropdown-menu user-pro-dropdown">
+
+                    <a href="pages-profile.html" class="dropdown-item notify-item">
+                        <i data-feather="user" class="icon-dual icon-xs me-1"></i><span>My Account</span>
+                    </a>
+                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                        <i data-feather="settings" class="icon-dual icon-xs me-1"></i><span>Settings</span>
+                    </a>
+                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                        <i data-feather="help-circle" class="icon-dual icon-xs me-1"></i><span>Support</span>
+                    </a>
+                    <a href="pages-lock-screen.html" class="dropdown-item notify-item">
+                        <i data-feather="lock" class="icon-dual icon-xs me-1"></i><span>Lock Screen</span>
+                    </a>
+                    <div class="dropdown-divider"></div>
+                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                        <i data-feather="log-out" class="icon-dual icon-xs me-1"></i><span>Logout</span>
+                    </a>
+
+                </div>
+            </div>
+            <p class="text-muted">Admin Head</p>
+        </div>
+
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+
+            <ul id="side-menu">
+
+                <!-- <li class="menu-title">Navigation</li> -->
+                <li>
+                    <a href="{{ route('home') }}">
+                        <i data-feather="home"></i>
+                        <span> Dashboard </span>
+                    </a>
+                </li>
+
+            </ul>
+
+        </div>
+        <!-- End Sidebar -->
+
+        <div class="clearfix"></div>
+
+    </div>
+    <!-- Sidebar -left -->
+
+</div>

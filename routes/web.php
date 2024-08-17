0779d5794a9985fd73a0f9a1f04fa96ace198f35
Rio Anugrah Adam Saputra <rioanugrah999@gmail.com>
2024-08-17 13:48:46 +0700

    Inspeksi Mobil

--- routes/web.php
+++ routes/web.php
@@ -89,4 +89,8 @@ Route::domain(parse_url(env('APP_URL'), PHP_URL_HOST))->group(function () {
 
         });
     });
+    Route::get('testinghome', function(){
+       return ini_get('post_max_size');
+    });
+
 });

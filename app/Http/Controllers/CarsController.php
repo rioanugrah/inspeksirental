8c3f2f2df46d945affaf5b7cb93bbd180bf9947d
Rio Anugrah Adam Saputra <rioanugrah@Rios-MacBook-Air.local>
2025-11-04 20:36:37 +0700

    Inspeksi Rental

--- app/Http/Controllers/CarsController.php
+++ app/Http/Controllers/CarsController.php
@@ -1396,9 +1396,9 @@ class CarsController extends Controller
 
         foreach ($request['group-a'] as $key => $value) {
             // $inputBody[$key] = $value;
-            // $image_foto_lain_lain = $value['foto_lain_lain'];
-            $image_foto_lain_lain = $request['foto_lain_lain_'.$key];
-            dd($image_foto_lain_lain);
+            $image_foto_lain_lain = $value['foto_lain_lain'];
+            // $image_foto_lain_lain = $request['foto_lain_lain_'.$key];
+            // dd($image_foto_lain_lain);
             $img_foto_lain_lain = \Image::make($image_foto_lain_lain->path());
 
             $img_foto_lain_lain->resize(800, null, function ($constraint) {
